92b87686bdef4d2c228a22cfe64fa03036e3fc2e
Jan Brezina <jan.brezina@tul.cz>
2017-08-28 16:56:31 +0200

    Several improvements of the main converter script
    - save original files instead of new name for result
    - support for undo conversions
    - support for dry-run
    - optional report of conversion actions
    - fixed wildcard expansion

--- yaml_converter.py
+++ yaml_converter.py
@@ -39,35 +39,60 @@ if __name__ == "__main__":
             # absloute
             dirs = ["/"]
 
-        files = []
-        for wild_name in path_wild:
-            # separate dirs and files
+        # expand dirs
+        for wild_name in path_wild[:-1]:
             paths = []
             for dir in dirs:
                 paths += [dir + "/" + f for f in os.listdir(dir) if fnmatch.fnmatch(f, wild_name)]
             dirs = []
             for path in paths:
                 if os.path.isdir(path):
-                    print("dir append: %s"%path)
                     dirs.append(path)
-                elif os.path.isfile(path):
-                    print("file append: %s"%path)
-                    if not path.endswith(".new.yaml"):
-                        files.append(path)
-                else:
-                    assert False, "Path neither dir nor file."
+
+        # expand files
+        wild_name = path_wild[-1]
+        paths = []
+        for dir in dirs:
+            paths += [dir + "/" + f for f in os.listdir(dir) if fnmatch.fnmatch(f, wild_name)]
+
+        files = []
+        for path in paths:
+            if os.path.isfile(path):
+                files.append(path)
         return files
 
 
-    logging.basicConfig(stream=sys.stdout, level=logging.WARNING)
+    logging.basicConfig(stream=sys.stdout, level=logging.INFO)
     from change_rules import make_changes
     changes = make_changes()
 
-    parser = argparse.ArgumentParser()
+    parser = argparse.ArgumentParser(
+        prog="yaml-converter",
+        description="Bidirectional conversion of YAML file(s) using given set of change rules.")
     #parser.add_argument("-f", "--from-version", default="0.0.0", help="Version of the input file. ")
-    parser.add_argument("-t", "--to-version", default="ZZ.ZZ.ZZ", help="Version of the output.")
-    parser.add_argument("-r", "--reverse", action='store_true', help="Perform reversed conversion. Input file is in 'to-version'.")
-    parser.add_argument('in_file', help="Input YAML (or CON) file(s). Wildcards accepted.")
+    parser.add_argument("-s", "--save-orig", default="orig",
+                        metavar='EXT',
+                        help="Giving the non-empty value the original file is renamed to *.EXT.yaml."
+                             "Proformed even for unchanged files in order to support --undo. These files are excluded from conversion. ")
+    parser.add_argument("-d", "--dry-run", action='store_true',
+                        help="Do not write the file after conversion.")
+    parser.add_argument("-t", "--to-version", default="ZZ.ZZ.ZZ",
+                        help="Version of the output.")
+    parser.add_argument("-r", "--reverse", action='store_true',
+                        help="Perform reversed conversion. Input file is in 'to-version'.")
+    parser.add_argument("-u", "--undo",
+                        dest='undo_level',
+                        type=int,
+                        const=-1,    # without value
+                        default=0,  # without option
+                        nargs='?',
+                        metavar="N",
+                        help="Undo N last conversions before proforming conversion. Undo conversions with same '--save-orig' value."
+                             "For N=-1 or without N undo all conversions. ")
+    parser.add_argument("--report-actions", action="store_true",
+                        help="Report used actions and fiels they are used in.")
+    parser.add_argument('in_file',
+                        help="Input YAML (or CON) file(s). Wildcards accepted.")
 
     args = parser.parse_args()
     files = expand_wild_pattern(args.in_file)
@@ -77,19 +102,80 @@ if __name__ == "__main__":
     action_files={}
     for fname in files:
         base = os.path.splitext(fname)[0]
-        if not fname.endwith(".yaml"):
-            print("Warning: '{}' is not \"*.yaml\", skipping.".format(fname))
-        with open(fname, "r") as f:
-            tree = yml.load(f)
-            actions = changes.apply_changes(tree, args.to_version, reversed=args.reverse, map_insert=Changes.BEGINNING)
-            for act in actions:
-                action_files.setdefault(act, {})
-                action_files[act][fname]=True
+        dir, filename = os.path.split(fname)
+        file_base = os.path.splitext(filename)[0]
 
-        out_fname = base + ".new.yaml"
-        with open(out_fname, "w") as f:
+        # skip non-yaml extensions
+        if not fname.endswith(".yaml"):
+            print("Warning: '{}' is not \"*.yaml\", skipping.".format(fname))
+            continue
+        orig_sufix = "." + args.save_orig + ".yaml"
+        if orig_sufix == "..yaml":
+            orig_sufix = None
+
+        # skip saved original files
+        if orig_sufix and fname.endswith(orig_sufix):
+            continue
+
+        def orig_split(f):
+            ff = f
+            prefix = file_base + "."
+            if not f.startswith(prefix):
+                return  None
+            f = f[len(prefix)-1 :]         # keep leading dot
+            if not f.endswith(orig_sufix):
+                return None
+            f = f[:-len(orig_sufix)]     # remove including dot
+            if len(f) == 0:
+                return (0, ff)
+            f = f[1:]                   # remove leading dot
+            try:
+                return (int(f), ff)
+            except:
+                return None
+
+        # perform undo
+        if args.undo_level != 0:
+            dir, filename = os.path.split(fname)
+            file_base = os.path.splitext(filename)[0]
+            wild_name = file_base + "*" + orig_sufix
+            parsed_files = ( orig_split(f) for f in os.listdir(dir) )
+            i_saved_files = [ f for f in parsed_files if not f is None ]
+            if i_saved_files:
+                i_saved_files.sort(reverse=True)
+                i, saved_files = zip(*i_saved_files)
+                revert_file = saved_files[args.undo_level]
+                for undo_file in saved_files[:args.undo_level]:
+                    os.remove(os.path.join(dir, undo_file))
+                os.remove(fname)
+                os.rename(os.path.join(dir, revert_file), fname)
+
+        with open(fname, "r") as f:
+            logging.info("Converting file: {}".format(fname))
+            tree = yml.load(f)
+            try:
+                actions = changes.apply_changes(tree, args.to_version, reversed=args.reverse, map_insert=Changes.BEGINNING)
+                for act in actions:
+                    action_files.setdefault(act, {})
+                    action_files[act][fname]=True
+            except:
+                raise Exception("Failed conversion of file: {}". format(fname))
+
+        if args.dry_run:
+            continue
+
+        # safe orig file to unused name
+        orig_fname = base + orig_sufix
+        i = 0
+        while os.path.isfile(orig_fname):
+            i += 1
+            orig_fname = base + "." + str(i) + orig_sufix
+
+        os.rename(fname, orig_fname)
+        with open(fname, "w") as f:
             yml.dump(tree, f)
 
-    action_list = [ (line, act, f_dict.keys()) for (act, line), f_dict in action_files.items()]
-    for l,a,f in sorted(action_list, key=lambda x:x[0]):
-        print( "Line: %4d Action: %12s Files: %s"%(l,a,f) )
+    if args.report_actions:
+        action_list = [ (line, act, f_dict.keys()) for (act, line), f_dict in action_files.items()]
+        for l,a,f in sorted(action_list, key=lambda x:x[0]):
+            print( "Line: %4d Action: %12s Files: %s"%(l,a,f) )
